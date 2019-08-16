106bba33cf77bc0b7d310e8a47db82c3b960677d
Vincent Mackay <mackv42@gmail.com>
2019-08-15 19:27:57 -0500

    rollback

--- src/LiveTunes.MVC/Controllers/EventController.cs
+++ src/LiveTunes.MVC/Controllers/EventController.cs
@@ -29,12 +29,22 @@ namespace LiveTunes.MVC.Controllers
             client = new HttpClient();
             _context = context;
 
+<<<<<<< HEAD
 			//if (context.Events.Count() <= 1)
 			//{
 			//	context.Events.Add(new Event { Latitude = 49.2746619, Longitude = -123.10921740000003, EventName = "King Gizzard and the Lizard Wizard", DateTime = DateTime.Now, Genre = "Post Punk" });
 			//	context.Events.Add(new Event { Latitude = 49.2746619, Longitude = -123.0451041, EventName = "King Gizzard and the Lizard Wizard", DateTime = DateTime.Now, Genre = "Post Punk" });
 			//}
 			//context.SaveChangesAsync();
+=======
+            if (context.Events.Count() <= 1)
+			{
+				context.Events.Add(new Event { Latitude = 49.2746619, Longitude = -123.10921740000003, EventName = "King Gizzard and the Lizard Wizard", DateTime = DateTime.Now, Genre = "Post Punk" });
+				context.Events.Add(new Event { Latitude = 49.2746619, Longitude = -123.0451041, EventName = "King Gizzard and the Lizard Wizard", DateTime = DateTime.Now, Genre = "Post Punk" });
+			}
+
+			context.SaveChangesAsync(); /*Comment this back out*/
+>>>>>>> parent of 1ff39db... filtered events by category_id
 		}
 
         [HttpPost]
@@ -43,8 +53,7 @@ namespace LiveTunes.MVC.Controllers
             List<Event> musicEvents = new List<Event>();
             List<Event> allEvents;
             try
-            {   
-                //EventBriteApi
+            {    
                 var result = await client.GetStringAsync($"https://www.eventbriteapi.com/v3/events/search?location.longitude={coordinate.Longitude}&location.latitude={coordinate.Latitude}&expand=venue&location.within=&token={EventbriteAPIToken.Token}");
 
                 var data = JsonConvert.DeserializeObject<JObject>(result);
@@ -95,18 +104,10 @@ namespace LiveTunes.MVC.Controllers
             RedirectToAction("Index");
         }
 
-        public async Task<IActionResult> Index()
+        public IActionResult Index()
         {
-
-            /*var results = await GetEvents();*/
-            /*var events = await _context.Events.FirstOrDefaultAsync();*/
-            var listOfGenres = await _context.MusicCategories.Where(x => true).ToListAsync();
-
-            return View(listOfGenres);
-
+            return View();
         }
-
-        
 
         public async Task<IActionResult> Details(int id)
         {
@@ -126,27 +127,6 @@ namespace LiveTunes.MVC.Controllers
             evnt.Comments = await _context.Comments.Where(x => x.EventId == id).ToListAsync();
 
             return View(evnt);
-        }
-
-
-        //get by preferences
-        public async Task List(Coordinate location)
-        {
-            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
-            var userProfileId = _context.UserProfiles.Where(x => x.UserId == userId).FirstOrDefault().UserProfileId;
-            var userProfile = await _context.UserProfiles.FirstOrDefaultAsync(x => x.UserId == userId);
-
-            // get User Preferences
-            // and query Event brite based on location
-            // query returned data based on preferences
-            return;
-        }
-
-
-        public async Task List(int GenreId, Coordinate location)
-        {
-            // api call to get List of events by genre and location
-            return;
         }
 
         [HttpPost]
