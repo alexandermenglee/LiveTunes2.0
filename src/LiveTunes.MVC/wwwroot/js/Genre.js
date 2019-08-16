6c2ad112317946d44295876300c573a4eceac799
Vincent Mackay <mackv42@gmail.com>
2019-08-16 14:09:11 -0500

    genre shows up in event details page

--- src/LiveTunes.MVC/wwwroot/js/Genre.js
+++ src/LiveTunes.MVC/wwwroot/js/Genre.js
@@ -1,29 +1,29 @@
-﻿var genreList = [];
-
+﻿genreList = [];
 function getGenreList() {
     $.ajax({
         type: "GET",
-        url: "musiccategory/list",
+        url: "/musiccategory/list",
         success(data) {
             //jquery to populate select list
             console.log(data);
+            genreList = data;
             var genres = document.getElementsByClassName("genres");
-
+           
             for (var j = 0; j < genres.length; j++) {
                 for (var i = 0; i < data.length; i++) {
-                    genreList.push({ id: data[i].Id, name: data[i].categoryName });
-                    genreList[i].categoryName = data[i].categoryName;
-                    genreList[i].id = data[i].id;
+                    //genreList.push({ id: data[i].Id, name: data[i].categoryName });
+                    
                     genres[j].innerHTML += "<option value=" + data[i].id + ">" + data[i].categoryName + "</option>"
                 }
             }
         }
     });
 }
+getGenreList();
+function getGenre(id) {
+    //console.log(genreList + "genres here");
 
-function getGenre( id ) {
-    let r = genreList.filter(x => x.id == id)[0];
-    return r.categoryName;
+    var arr = genreList.filter(x => x.id == id);
+    return arr[0].categoryName;
 }
 
-getGenreList();
