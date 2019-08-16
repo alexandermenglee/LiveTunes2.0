957a6839354541c43a34d41dae7314e3a41717c2
Vincent Mackay <mackv42@gmail.com>
2019-08-16 08:30:10 -0500

    pull

--- src/LiveTunes.MVC/Data/Migrations/20190815175215_addMusicCategories.cs
+++ src/LiveTunes.MVC/Data/Migrations/20190815175215_addMusicCategories.cs
@@ -1,17 +1,17 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+﻿//using Microsoft.EntityFrameworkCore.Migrations;
 
-namespace LiveTunes.MVC.Migrations
-{
-    public partial class addMusicCategories : Migration
-    {
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
+//namespace LiveTunes.MVC.Migrations
+//{
+//    public partial class addMusicCategories : Migration
+//    {
+//        protected override void Up(MigrationBuilder migrationBuilder)
+//        {
 
-        }
+//        }
 
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
+//        protected override void Down(MigrationBuilder migrationBuilder)
+//        {
 
-        }
-    }
-}
+//        }
+//    }
+//}
---
+++ src/LiveTunes.MVC/GoogleMapsAPIToken(1).cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LiveTunes.MVC
+{
+    public static class GoogleMapsAPIToken
+    {
+        public static string Token { get => "AIzaSyBJI2go_Ooa47HozynHbq9LNm-qqP-rvz0";  }
+        
+    }
+}
