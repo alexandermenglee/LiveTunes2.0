996405640ccf221370a692921e8e6f9345e4819b
Ridvan Ermaya <ridvanermaya@gmail.com>
2019-08-18 23:53:12 -0500

    Removed unncessary lines of code

--- src/LiveTunes.MVC/Controllers/EventController.cs
+++ src/LiveTunes.MVC/Controllers/EventController.cs
@@ -29,25 +29,18 @@ namespace LiveTunes.MVC.Controllers
         {
             client = new HttpClient();
             _context = context;
-
-            //if (context.Events.Count() <= 1)
-            //{
-            //	context.Events.Add(new Event { Latitude = 49.2746619, Longitude = -123.10921740000003, EventName = "King Gizzard and the Lizard Wizard", DateTime = DateTime.Now, Genre = "Post Punk" });
-            //	context.Events.Add(new Event { Latitude = 49.2746619, Longitude = -123.0451041, EventName = "King Gizzard and the Lizard Wizard", DateTime = DateTime.Now, Genre = "Post Punk" });
-            //}
-            //context.SaveChangesAsync();
         }
 
 
         private double getDistance(Coordinates g1, Coordinates g2)
         {
-
             return g1.DistanceTo(g2);
         }
 
         public async Task<List<Event>> getEventsByDistance(Coordinate coordinate, double d)
         {
             var sCoord = new Coordinates(double.Parse(coordinate.Latitude), double.Parse(coordinate.Longitude));
+
             return await _context.Events.Where(x => getDistance(sCoord, new Coordinates(x.Latitude, x.Longitude)) < d).ToListAsync();
         }
 
@@ -62,6 +55,7 @@ namespace LiveTunes.MVC.Controllers
         public async Task<List<Event>> GetEventsByCoordinates(Coordinate coordinate)
         {
             List<Event> returnEvents = new List<Event>();
+
             try
             {
                 var result = await client.GetStringAsync($"https://www.eventbriteapi.com/v3/events/search?location.longitude={coordinate.Longitude}&location.latitude={coordinate.Latitude}&expand=venue&location.within=50km&token={EventbriteAPIToken.Token}");
@@ -81,6 +75,7 @@ namespace LiveTunes.MVC.Controllers
                     EVENTS = data["events"].Where(e => (string)e["category_id"] == "103").ToList();
 
                     Event add = new Event();
+
                     for (int i = 0; i < EVENTS.Count; i++)
                     {
                         var eventFromDB = _context.Events.Where(e => e.EventbriteEventId.Equals((string)EVENTS[i]["id"])).FirstOrDefault();
@@ -111,11 +106,9 @@ namespace LiveTunes.MVC.Controllers
                         {
                             newEvent.Genre = int.Parse((string)EVENTS[i]["subcategory_id"]);
                         }
-
                         await _context.Events.AddAsync(newEvent);
                     }
                 }
-
                 await _context.SaveChangesAsync();
             }
             catch (HttpRequestException e)
@@ -126,10 +119,8 @@ namespace LiveTunes.MVC.Controllers
             return returnEvents;
         }
 
-
         [HttpPost]
         public async Task<List<Event>> Handoff([FromBody] Coordinate coordinate)
-
         {
             //return await GetEventsByCoordinates(coordinate);
             return await getEventsByDistance(coordinate, 30);
@@ -157,7 +148,6 @@ namespace LiveTunes.MVC.Controllers
 
             ViewBag.UserFirstName = userProfile.FirstName;
             ViewBag.UserLastName = userProfile.LastName;
-
 
             evnt.LikeCount = await _context.Likes.CountAsync(x => x.EventId == id);
             evnt.CommentCount = await _context.Comments.CountAsync(x => x.EventId == id);
@@ -198,6 +188,7 @@ namespace LiveTunes.MVC.Controllers
             var userProfileId = userProfile.UserProfileId;
 
             var like = await _context.Likes.FirstOrDefaultAsync(x => x.UserId == userProfileId && x.EventId == id);
+
             if (like != null)
             {
                 _context.Likes.Remove(like);
@@ -229,8 +220,8 @@ namespace LiveTunes.MVC.Controllers
             var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
             var userProfile = await _context.UserProfiles.FirstOrDefaultAsync(x => x.UserId == userId);
             var userProfileId = userProfile.UserProfileId;
-
             var newComment = new Comment();
+            
             newComment.DateTime = DateTime.Now;
             newComment.UserId = userProfileId;
             newComment.EventId = evnt.EventId;
