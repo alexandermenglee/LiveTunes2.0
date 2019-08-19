d57cf8266a273f7f74d1396407b1c49546daac29
Luke Fleuri <lukefleuri@gmail.com>
2019-08-18 21:39:21 -0500

    Update pull

---
+++ src/LiveTunes.MVC/Controllers/UserProfileController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using LiveTunes.MVC.Data;
+using LiveTunes.MVC.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LiveTunes.MVC.Controllers
+{
+    public class UserProfileController : Controller
+    {
+        private ApplicationDbContext _context;
+        public UserProfileController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        // GET: UserProfile
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: UserProfile/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: UserProfile/Create
+        public ActionResult Create()
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var newUserProfile = _context.UserProfiles.FirstOrDefault(x => x.UserId == userId);
+
+            if (newUserProfile != null)
+            {
+                return RedirectToAction("Create", "Survey");
+            }
+
+            return View();
+        }
+
+        // POST: UserProfile/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(UserProfile profile)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var newUserProfile = new UserProfile();
+                
+            newUserProfile.UserId = userId;
+            newUserProfile.FirstName = profile.FirstName;
+            newUserProfile.LastName = profile.LastName;
+
+            _context.UserProfiles.Add(newUserProfile);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Create", "Survey"); 
+        }
+
+        // GET: UserProfile/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: UserProfile/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            /*try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }*/
+            return View();
+        }
+
+        // GET: UserProfile/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: UserProfile/Delete/5
+        
+    }
+}
